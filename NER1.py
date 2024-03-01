a58a519d127f31c7fd1ff1cef4dd6f7804b9f735
shuai20 <2586047455@qq.com>
2024-03-01 14:22:19 +0100

    Load dataset2,3

--- NER1.py
+++ NER1.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 # load dataset
-data = pd.read_csv("sentence1.csv")
+data1 = pd.read_csv("sentence1.csv")
 
 # check dataset
-print(data.head())
+print(data1.head())
+data2 = pd.read_csv("sentence2.csv")
+data3 = pd.read_csv("sentence3.csv")
